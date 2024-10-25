d8e1dd008d1eb9acf2354d955d4c0cf5c1bc62fe
angeladso <angeladso@gmail.com>
2024-10-25 22:50:05 +0200

    1_MVC_mostrarproductos

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'configuracion/conexion.php';
+require_once 'controladores/ProductoControlador.php';
+
+$controladorProducto = new ProductoControlador();
+$controladorProducto->mostrarProductos();
+?>
